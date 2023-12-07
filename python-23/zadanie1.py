3806e0e8c4282f2eb73e5450fa4c6abcac212ebf
Tinka8 <lenmatka818@gmail.com>
2023-12-07 17:19:18 +0100

    feat: version 1.0

--- python-23/zadanie1.py
+++ python-23/zadanie1.py
@@ -1,5 +1,4 @@
 user_input = str(input())
-print(f"user_input {user_input}")
 
 # initiate variables to store values
 summ = 0 
@@ -25,19 +24,12 @@ index += summ
 # get first chars based on sum of first 2 digits
 result = user_input[:index]
 
-print(f"result prvy {result}")
-
 # store pin num
 final_result = result[summ-1]
 pin_number += final_result
 
 # create new string using remaining chars 
 new_string = user_input[index:] 
-
-print(f"first string {new_string}")
-print(f"first pin num {pin_number}")
-
-print("---------------")
 
 
 
@@ -50,13 +42,8 @@ while len(new_string) > 0:
 
     # get first 2 digits
     first_two = new_string[:2]
-    print(f"first two {first_two}")
     if '0' in first_two:
-        index = 1
-    
-
-    print(f"chytena nula?? {index}")
-    
+        index = 1  
 
     # get sum of first 2 digits 
     for i in first_two:
@@ -70,45 +57,26 @@ while len(new_string) > 0:
         
     # num of chars removed from string 
     index += summ
-    print("=====")
-    print(index)
-    print(summ)
-    print("=====")
-  
     
     if summ > len(new_string):
         break
 
 
     # get first chars based on sum of first 2 digits
-    print(f"index pred novym stringom {index}")
     result = new_string[:index]
    
 
-    #####
-
-  
-
     # store pin num
-    print(f"result ======= {result}")
-
-    
     final_result = result[summ-1]
-    print(f"final result {final_result}")
     pin_number += final_result
     
 
     # create new string using remaining chars 
     new_string = new_string[index:] 
-    print(f"create new string using remaining chars  {new_string}")
 
 
     if len(new_string) == 0:
         break
     
-    
-    # prints
-    print(f"PIN num {final_result}")
-    print(f"new string {new_string}")
 
-print(f"FINAL PIN num {pin_number}")
+print(f"{pin_number}")
