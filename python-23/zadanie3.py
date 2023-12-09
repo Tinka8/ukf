f3aeb3b5a0a4e0b8853e58e705e2d9994501b9f7
Tinka8 <lenmatka818@gmail.com>
2023-12-09 18:49:56 +0100

    feat: version 0.1

--- python-23/zadanie3.py
+++ python-23/zadanie3.py
@@ -1,5 +1,5 @@
-capacity = 10
-user_input = "20,0,20,5,10,10,5,0"
+capacity = 30
+user_input = "35,0,10,20,10,20"
 
 # split user input into array
 array = user_input.split(",")
@@ -7,7 +7,6 @@ input_array = array[::2]
 output_array = array[1::2]
 
 length = len(input_array)
-print(length)
 
 print(f"input {input_array}")
 print(f"output {output_array}")
@@ -15,14 +14,26 @@ print(f"output {output_array}")
     
 input_num = 0
 output_num = 0
+current_capacity = 0
+current = 0
+
 
 i = 0
 while i < len(input_array):
-    print(i)
-    print(f"input {input_array[i]}")
-    print(f"output {output_array[i]}")
-        
+
+    current_capacity = int(input_array[i]) - int(output_array[i])
+    current += current_capacity
+    
+    if current > capacity:
+        print("vysledok -----")
+        print(i, current - capacity)
+   
+
     i += 1
     
+    if i == len(input_array) and current < capacity:
+        print("ani raz")
+        break
     
-        
+    
+
